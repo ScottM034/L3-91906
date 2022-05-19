5fee595973fbbd637f43abd169b9a53b72d52b90
ScottM034 <103980107+ScottM034@users.noreply.github.com>
2022-05-19 12:53:48 +1200

    Add files via upload
    V1

---
+++ jobmakingapp.py
@@ -0,0 +1,185 @@
+from tkinter import*
+import tkinter.font as font
+from random import*
+import time
+
+root = Tk()
+root.geometry("600x600")
+
+def student_page():
+    root.destroy()
+    import studentpage
+def reference_page():
+    root.destroy()
+    import referencepage
+def employer_page():
+    root.destroy()
+    import employerpage
+
+def login():
+    username = e1.get()
+    password = e2.get()
+    act_type = con.get()
+    u_p = username+password+str(act_type) 
+    try:
+        f = open("Usernames.txt", "x")
+        g = open("Passwords.txt", "x")
+    except FileExistsError:
+        pass
+    open_u = open("Usernames.txt")
+    open_p = open("Passwords.txt")
+    if u_p in open_u.read() and act_type == 1:
+        try:
+            f = open("Logged.txt", "x")
+        except FileExistsError:
+            pass
+        write_logs = open("Logged.txt", "w")
+        write_logs.write(username+password+"1"+"\n")
+        write_logs.close
+        student_page()
+    if u_p in open_u.read() and act_type == 2:
+        try:
+            f = open("Logged.txt", "x")
+        except FileExistsError:
+            pass
+        write_logs = open("Logged.txt", "w")
+        write_logs.write(username+password+"2"+"\n")
+        write_logs.close
+        reference_page()
+    if u_p in open_u.read() and act_type == 3:
+        try:
+            f = open("Logged.txt", "x")
+        except FileExistsError:
+            pass
+        write_logs = open("Logged.txt", "w")
+        write_logs.write(username+password+"3"+"\n")
+        write_logs.close
+        employer_page()
+    else:
+        lbl25 = Label(root, text="That account doesn't exist")
+        lbl25.grid(row = 13, column = 3, columnspan=6)
+    
+        
+def signup():
+    username = e3.get()
+    password = e4.get()
+    if len(password) < 8:
+        lbl10 = Label(root, text="Password should be 8 characters or more", width = 42, fg='#FF0000')
+        lbl10.grid(row = 24, column = 3, columnspan=6, sticky='W')
+    else:
+        try:
+            f = open("Usernames.txt", "x")
+            g = open("Passwords.txt", "x")
+        except FileExistsError:
+            pass
+        if username in open("Usernames.txt","r"):
+            lbl10 = Label(root, text="Username already exists, please try again", width = 30,fg='#FF0000')
+            lbl10.grid(row = 26, column = 33)
+            pass    
+        else:
+            write_users = open("Usernames.txt", "a")
+            write_users.write(username+password+(str(var.get()))+"\n")
+            write_users.close
+            if var.get() == 1:
+                try:
+                    f = open("Logged.txt", "x")
+                except FileExistsError:
+                    pass
+                write_logs = open("Logged.txt", "w")
+                write_logs.write(username+password+"1"+"\n")
+                write_logs.close
+                student_page()
+            elif var.get() == 2:
+                try:
+                    f = open("Logged.txt", "x")
+                except FileExistsError:
+                    pass
+                write_logs = open("Logged.txt", "w")
+                write_logs.write(username+password+"2"+"\n")
+                write_logs.close
+                reference_no = str(randint(1,1000000))
+                try:
+                    open(username+".txt", "x")
+                except FileExistsError:
+                    pass
+                reference_write = open(username+".txt", "a")
+                reference_write.write("ReferenceNo"+reference_no)
+                reference_write.close()
+                try:
+                    f = open("References.txt", "x")
+                except FileExistsError:
+                    pass
+                write_r = open("References.txt", "w")
+                write_r.write(username+"\n")
+                write_r.close
+                reference_page()
+            elif var.get() == 3:
+                try:
+                    f = open("Logged.txt", "x")
+                except FileExistsError:
+                    pass
+                write_logs = open("Logged.txt", "w")
+                write_logs.write(username+password+"3"+"\n")
+                write_logs.close 
+                employer_page()
+            else:
+                print("Something went wrong")
+
+  
+f1 = font.Font(family='Times New Roman')
+f2 = font.Font(family='Times New Roman', size = 12)
+var = IntVar()
+con = IntVar()
+
+lbl1 = Label(root, text="Login", width = 15, font = f1)
+lbl1.grid(row = 3, column = 4)
+lbl2 = Label(root, text="Name :", width = 15, font = f1)
+lbl2.grid(row = 8, column = 2)
+e1 = Entry(root, font =('Arial', 12), width = 30)
+e1.grid(row = 8, column = 3, columnspan=6)
+lbl21 = Label(root, text="", width=1)
+lbl21.grid(row = 9, column = 1)
+lbl3 = Label(root, text="Password :", width = 15, font = f1)
+lbl3.grid(row = 10, column = 2)
+e2 = Entry(root, font =('Arial', 12), width = 30)
+e2.grid(row = 10, column = 3, columnspan=6)
+lbl24 = Label(root, text=" ", width=1)
+lbl24.grid(row = 11, column = 1)
+lbl6 = Label(root, text = "Account Type :", font = f1)
+lbl6.grid(row=12, column = 2)
+r4 = Radiobutton(root, text = "Student", variable=con, value = 1)
+r4.grid(row = 12, column = 3)
+r5 = Radiobutton(root, text = "Reference", variable=con, value = 2)
+r5.grid(row = 12, column = 4)
+r6 = Radiobutton(root, text = "Teacher", variable=con, value = 3)
+r6.grid(row = 12, column = 5)
+btn1 = Button(root, text="Login", font = f1, command=login)
+btn1.grid(row = 14, column = 4)
+lbl23 = Label(root, text="", width=1)
+lbl23.grid(row = 15, column = 1)
+lbl4 = Label(root, text="Sign-Up", width = 15, font = f1)
+lbl4.grid(row = 18, column = 4)
+lbl5 = Label(root, text="Name :", width = 15,font = f1)
+lbl5.grid(row = 23, column = 2)
+e3 = Entry(root, font =('Arial', 12), width = 30)
+e3.grid(row = 23, column = 3, columnspan=6)
+lbl22 = Label(root, text=" ", width=1)
+lbl22.grid(row = 24, column = 1)
+lbl6 = Label(root, text="Password :", width = 15, font = f1)
+lbl6.grid(row = 25, column = 2)
+e4 = Entry(root, font =('Arial', 12), width = 30)
+e4.grid(row = 25, column = 3, columnspan=6)
+btn2 = Button(root, text="Sign-Up", font = f1, command=signup)
+btn2.grid(row = 28, column = 4)
+lbl24 = Label(root, text=" ", width=1)
+lbl24.grid(row = 26, column = 1)
+lbl6 = Label(root, text = "Account Type :", font = f1)
+lbl6.grid(row = 27, column = 2)
+r1 = Radiobutton(root, text = "Student", variable=var, value = 1)
+r1.grid(row = 27, column = 3)
+r2 = Radiobutton(root, text = "Reference", variable=var, value = 2)
+r2.grid(row = 27, column = 4)
+r3 = Radiobutton(root, text = "Employer", variable=var, value = 3)
+r3.grid(row = 27, column = 5)
+
+root.mainloop()
