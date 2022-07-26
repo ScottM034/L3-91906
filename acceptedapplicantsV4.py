ec0d076dcd6cfd424c20932305c0f1dbe3a84f39
ScottM034 <103980107+ScottM034@users.noreply.github.com>
2022-07-27 11:53:35 +1200

    Add files via upload

---
+++ acceptedapplicantsV4.py
@@ -0,0 +1,52 @@
+from tkinter import*
+root = Tk()
+root.geometry("500x500")
+# Sets the size of the page
+scrollbar = Scrollbar(root)
+# Creates a scrollbar the size of the page
+scrollbar.pack(side=RIGHT, fill=Y)
+# Puts the scrollbar on the right side of the page
+mylist = Listbox(root, width=500, yscrollcommand=scrollbar.set)
+# Creates a list of items with the scrollbar controlling it
+try:
+    open('Logged.txt', 'x')
+    # This section just checks if this external file exists
+except FileExistsError:
+    pass
+with open("Logged.txt") as h:
+    lines = h.read()
+    username = lines.split('\n', 0)[0].strip()
+    username = username[:-1]
+    # This gets the first line of the file and removes the last letter
+    try:
+        open(username + "Accepted.txt", "x")
+    except FileExistsError:
+        pass
+    file = open(username + "Accepted.txt", "r")
+    x = file.readlines()  # Reads the file and sets each line as a list
+for i in range(len(x)):  # Repeats this loop for the length of the file
+    if i == 0:
+        mylist.insert(END, "YOUR ACCEPTED APPLICANTS")
+        mylist.insert(END, "     ")
+    else:
+        pass
+    if "\n" in x[i]:
+        # Only does the loop if the line goes onto a new line to
+        # make sure empty lines are not read
+        student_page = open(x[i][:-1]+"S.txt")
+        student_info = student_page.readlines()
+        mylist.insert(END, x[i][:-1])
+        # This adds the student name at the end of this scrolling list
+        for j in range(len(student_info)):
+            mylist.insert(END, student_info[j])
+            # Adds that student's information to the end of that list
+        mylist.insert(END, "     ")
+        # Just adds an extra line for space
+    else:
+        pass
+mylist.pack(side=LEFT, fill=BOTH)
+# Moves the list to stay on the left and fill the page if required
+scrollbar.config(command=mylist.yview)
+# Tells the scrollbar to make it scroll vertically
+
+root.mainloop()
---
+++ studentpageV4.py
@@ -0,0 +1,55 @@
+from importlib import reload
+from tkinter import*
+root = Tk()
+root.geometry("500x500")
+# Sets the size of the window
+scrollbar = Scrollbar(root)
+# Creates a scrollbar around the window
+scrollbar.pack(side=RIGHT, fill=Y)
+# Puts the scrollbar on the right side of the window
+mylist = Listbox(root, width=500, yscrollcommand=scrollbar.set)
+# Makes the window a list of text
+try:
+    open("Employers.txt", "x")  # Checks if this file exists
+except FileExistsError:
+    pass
+file = open("Employers.txt", "r")
+x = file.readlines()
+# Creates a python list from everything in the external file
+for i in range(len(x)):
+    if i == 0:
+        mylist.insert(END, "COMPANY POSTINGS")
+        mylist.insert(END, "     ")
+    else:
+        pass
+    existing_check = 0
+    employer_name = x[i][:-1]
+    try:
+        open(employer_name + "3.txt")
+    except FileNotFoundError:
+        existing_check = 1
+        # This checks if the company is complete and is ready to be applied for
+    if existing_check == 0:
+        with open(employer_name+"3.txt") as j:
+            top = j.read()
+            topline = top.split('\n', 0)[0]
+            # Gets the first line in that file which is the reference number
+        employer_name = employer_name.strip("1")
+        # Gets the employer name without the type
+        employer_page = open(employer_name+"E.txt")
+        employer_info = employer_page.readlines()
+        # Gets the list of the company's information
+        mylist.insert(END, employer_name)
+        mylist.insert(END, "Reference Number: " + topline.strip("RefNo"))
+        # Adds the company's name and refernce number to this scrolling list
+        for j in range(len(employer_info)):
+            mylist.insert(END, employer_info[j])
+            # Adds the company's info to the list
+        mylist.insert(END, "     ")
+    else:
+        pass
+mylist.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=mylist.yview)
+# Tells the scrollbar that when scrolled it goes vertically
+
+root.mainloop()
